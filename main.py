efeadd2b79d9033567af45cf474f11ab37ed6264
mmriyahi7337 <mmriyahi7337@yahoo.com>
2024-03-26 08:05:54 +0330

    add ips unassign and assign ip section update todos and findout sum needs

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import os
+import time
+
 import requests
 from dotenv import load_dotenv
 from hcloud import Client
 from hcloud.images import Image
 from hcloud.server_types import ServerType
+from hcloud.servers import ServersClient
+from hcloud.primary_ips import PrimaryIPsClient
 
 load_dotenv()
 Token = os.getenv("Token")
 
 client = Client(token=Token)
-#TODO: add check host section
+serverclient = ServersClient(client=client)
+primaryips = PrimaryIPsClient(client=client)
 
+
+# TODO: tidy up code
 def testip(ip):
     url = f"https://check-host.net/check-tcp?host={ip}:22"
 
@@ -50,6 +57,23 @@ def testip(ip):
             return False
 
 
+# TODO: error handle for this section
+def unaasign_ip(server, primaryip):
+    response = serverclient.power_off(server)
+    time.sleep(10)
+    rs = primaryips.unassign(primaryip)
+
+
+# TODO : error handle
+# TODO : datacenters loc id or name
+def assign_ip(server, primaryip, datacenter):
+    # rs_ip = primaryips.create(name="primary", type="ipv4", datacenter=datacenter)
+    rs_ip = primaryips.get_by_name("primary")
+    pp = primaryips.assign(primary_ip=rs_ip, assignee_id=server.id)
+    serverclient.power_on(server)
+    return pp
+
+
 def create_vps():
     response = client.servers.create(
         name="my-server",
@@ -57,18 +81,34 @@ def create_vps():
         image=Image(name="ubuntu-22.04"),
     )
     server = response.server
-    print(f"{server.id=} {server.ip=} {server.status=}")
-    print(f"root password: {response.root_password}")
+    # print(f"{server.id=} {server.ip=} {server.status=}")
+    # print(f"root password: {response.root_password}")
+    return server
+
+
 # TODO: add multi server for checking or multi tread
 
 
+# TODO: add a var for number of ip check or clean ip
+# TODO: add array for holding ips and counter
+# TODO: delete the extra ips
 
-#TODO: add a var for number of ip check or clean ip
-#TODO: add array for holding ips and counter
-
-def show_vps_inf():
-    servers = client.servers.get_by_id(42819738)
-    print(servers.public_net.ipv4.ip)
+# TODO: delete this section
+def show_vps_inf(id):
+    servers = client.servers.get_by_id(id)
+    return servers.public_net.ipv4.ip
 
 
-show_vps_inf()
+# server = create_vps()
+server = serverclient.get_by_id(45012665)
+# server_ip_by_id = server.public_net.primary_ipv4.id
+d = primaryips.get_by_id(54841603)
+dc = d.datacenter
+# print(server_ip_by_id)
+# print(server_id)
+# server_id = '45012095'
+# ip = show_vps_inf(server.id)
+# print(testip(ip))
+# time.sleep(30)
+# print(unaasign_ip(server,d))
+print(assign_ip(server, d, dc))
